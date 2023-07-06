e33f3a5bd306ab3b42544170f6b5c683119a6e01
Carol Wargo <Carolwargo@gmail.com>
2023-07-05 20:55:11 -0400

    added footer.jsx and styling

---
+++ coada4cb8ad51d11c1ced5c6f/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+    return(
+        <footer class="footer bg-dark text-white">
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        &copy; 2023 <div id="VANLIFE"></div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+    )
+}
