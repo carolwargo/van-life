d526c1706624d844a05ea1dcf5ba0b4637405812
Carol Wargo <Carolwargo@gmail.com>
2023-07-05 21:00:28 -0400

    fixed footer

--- coada4cb8ad51d11c1ced5c6f/components/Footer.jsx
+++ coada4cb8ad51d11c1ced5c6f/components/Footer.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Footer() {
     return(
-        <footer class="footer bg-dark text-white">
-  <div class="container">
-    <div class="row">
-      <div class="col text-center">
-        &copy; 2023 <div id="VANLIFE"></div>
-      </div>
-    </div>
-  </div>
-</footer>
+        
+        <footer>&#169; 2022 #VANLIFE</footer>
 
     )
 }
