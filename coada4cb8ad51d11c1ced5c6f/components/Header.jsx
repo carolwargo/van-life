7e6d2fa806dde095daa5a563cf4f06722756cbd4
Carol Wargo <Carolwargo@gmail.com>
2023-07-05 21:58:49 -0400

    added IsActive link styling

--- coada4cb8ad51d11c1ced5c6f/components/Header.jsx
+++ coada4cb8ad51d11c1ced5c6f/components/Header.jsx
@@ -8,16 +8,14 @@ export default function Header() {
            {/*//NavLink allows us to style using className or inline prop 
             //Add to the NavLink component: className={({isActive}) => isActive ? "my-link" : null } */} 
             <NavLink className="site-logo" to="/">#VanLife</NavLink>
-            <nav>
-                <NavLink to="/host">Host</NavLink>
-                <NavLink 
-          to="/about"
-          className={({isActive}) => isActive ? "my-link" : null }
-        >
-          About
-        </NavLink>
 
-                <NavLink to="/vans">Vans</NavLink>
+            <nav>
+                <NavLink to="/host"className={({isActive}) => isActive ? "active-link" : null }>Host</NavLink>
+                
+                <NavLink to="/about"className={({isActive}) => isActive ? "active-link" : null }>About</NavLink>
+
+                <NavLink to="/vans"className={({isActive}) => isActive ? "active-link" : null }>Vans</NavLink>
+                
             </nav>
         </header>
     )
