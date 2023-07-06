50ba381fd0e265fe71aa6fa8dff9c4ebfc1a53a9
Carol Wargo <Carolwargo@gmail.com>
2023-07-05 22:13:49 -0400

    imported HostVanDetail index.js

--- coada4cb8ad51d11c1ced5c6f/index.jsx
+++ coada4cb8ad51d11c1ced5c6f/index.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import HostVans from './pages/Host/HostVans';
+import HostVanDetail from './pages/Host/HostVanDetail';
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
@@ -30,7 +31,7 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetails />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
 
 
