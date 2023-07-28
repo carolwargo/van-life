d5408c283dee5623d3b0644b6c0aef70b2c621f2
Carol Wargo <Carolwargo@gmail.com>
2023-07-28 10:58:48 -0400

    update installs

--- coada4cb8ad51d11c1ced5c6f/pages/Host/HostVanDetail.jsx
+++ coada4cb8ad51d11c1ced5c6f/pages/Host/HostVanDetail.jsx
@@ -1,11 +1,17 @@
 import React from "react";  
-import { useParams } from "react-router-dom";   
+import { Link, useParams } from "react-router-dom";   
 
 export default function HostVanDetail() {
     {/*id vans:id from index.jsx */}
     const {id} = useParams()
     const [currentVan, setCurrentVan] = React.useState(null)
     
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     {/* Match above params "id vans:id from index.jsx" */}
     React.useEffect(() => {
          
@@ -19,12 +25,60 @@ export default function HostVanDetail() {
         return <h1>Loading...</h1>
     }
 
-    return(
-        <div>
-            <img src={currentVan.imageUrl} width={150} />
-            <h2>{currentVan.name}</h2>
-            <p>{currentVan.price}</p>
-            <p>{currentVan.type}</p>
-        </div>
-    )   
-}
+        return (
+            <section>
+                <Link
+                    to=".."
+                    relative="path"
+                    className="back-button"
+                >&larr; <span>Back to all vans</span></Link>
+    
+                <div className="host-van-detail-layout-container">
+                    <div className="host-van-detail">
+                        <img src={currentVan.imageUrl} />
+                        <div className="host-van-detail-info-text">
+                            <i
+                                className={`van-type van-type-${currentVan.type}`}
+                            >
+                                {currentVan.type}
+                            </i>
+                            <h3>{currentVan.name}</h3>
+                            <h4>${currentVan.price}/day</h4>
+                        </div>
+                    </div>
+    
+                    <nav className="host-van-detail-nav">
+                        <NavLink
+                            to="."
+                            end
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                        >
+                            Details
+                        </NavLink>
+                        <NavLink
+                            to="pricing"
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                        >
+                            Pricing
+                        </NavLink>
+                        <NavLink
+                            to="photos"
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                        >
+                            Photos
+                        </NavLink>
+                    </nav>
+                    <Outlet context={{ currentVan }} />
+                </div>
+            </section>
+        )
+    }
+    
+    /**
+     * Challenge: check out the docs linked in the slide, and see if you
+     * can implement the Outlet Context feature it talks about.
+     * 
+     * Part of this challenge will require you to (finally) build out those
+     * nested components. Again, if you don't need CSS practice, you can
+     * skip the styling part, and I'll handle that for you.
+     */
