04f38e6ce85507121227907330443415c13eec36
Carol Wargo <Carolwargo@gmail.com>
2023-09-01 17:14:17 -0400

    added error.jsx and images

--- coada4cb8ad51d11c1ced5c6f/components/Header.jsx
+++ coada4cb8ad51d11c1ced5c6f/components/Header.jsx
@@ -1,21 +1,46 @@
 import React from "react"
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+    
     return (
         <header>
-
-           {/*//NavLink allows us to style using className or inline prop 
-            //Add to the NavLink component: className={({isActive}) => isActive ? "my-link" : null } */} 
-            <NavLink className="site-logo" to="/">#VanLife</NavLink>
-
+            <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink to="/host"className={({isActive}) => isActive ? "active-link" : null }>Host</NavLink>
-                
-                <NavLink to="/about"className={({isActive}) => isActive ? "active-link" : null }>About</NavLink>
-
-                <NavLink to="/vans"className={({isActive}) => isActive ? "active-link" : null }>Vans</NavLink>
-                
+                <NavLink 
+                    to="host"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink 
+                    to="about"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink 
+                    to="vans"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src="../assets/images/avatar-icon.png" 
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
--- coada4cb8ad51d11c1ced5c6f/components/HostLayout.jsx
+++ coada4cb8ad51d11c1ced5c6f/components/HostLayout.jsx
@@ -13,11 +13,33 @@ export default function HostLayout() {
             <nav className="host-nav">
 
                 {/*inline styling and "end" prop to show IsActive route*/} 
-                <NavLink to="/host end "style={({isActive}) => isActive ? activeStyle : null }>Dashboard</NavLink>
-                <NavLink to="/host/income"style={({isActive}) => isActive ? activeStyle : null }>Income</NavLink>
-                <NavLink to="/host/reviews"style={({isActive}) => isActive ? activeStyle : null }>Reviews</NavLink>
-                <NavLink to="/host/vans"style={({isActive}) => isActive ? activeStyle : null }>Vans</NavLink>
-                {/*<NavLink to="/host/vans/:id"style={({isActive}) => isActive ? activeStyle : null }>HostVanDetail</NavLink>*/}
+                <NavLink 
+                    to="/host end "
+                    style={({isActive}) => isActive ? activeStyle : null }
+                    >
+                        Dashboard
+                    </NavLink>
+                <NavLink 
+                    to="/host/income"
+                    style={({isActive}) => isActive ? activeStyle : null }
+                    >
+                        Income
+                    </NavLink>
+                <NavLink 
+                    to="/host/reviews"
+                    style={({isActive}) => isActive ? activeStyle : null }
+                    >
+                        Reviews
+                    </NavLink>
+                <NavLink 
+                    to="/host/vans"
+                    style={({isActive}) => isActive ? activeStyle : null }
+                    >
+                        Vans
+                    </NavLink>
+                {/*<NavLink 
+                    to="/host/vans/:id"
+                    style={({isActive}) => isActive ? activeStyle : null }>HostVanDetail</NavLink>*/}
             </nav>
             <Outlet />
         </>
